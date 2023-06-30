cd40d75a6ee6cfa6fbdc75124116840fa75bb67a
Raviteja2003 <ravitejat0406@gmail.com>
2023-06-30 15:08:45 +0530

    enev file added

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import axios from "axios";
 import {useState} from "react";
-import { apiKey } from '../Api';
+
 const Content = () => {
    const [searchTerm, setSearchTerm] = useState("");
    const [data,setData]= useState({});
+   const apiKey = process.env.REACT_APP_API_KEY;
 
    const SearchHandler =()=>{
     if(!searchTerm)
